3ba1d92a8a764cf1ccc029837615106bc4025f6c
Ryan O'Shea <ryan.oshea@exitroute.org>
2020-10-18 23:46:42 +0200

    bug fix

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.prepare().then(() => {
   server.use(verifyRequest());
 
   server.use(async (ctx) => {
-    await handle(ctx.req, ctx, ctx.res);
+    await handle(ctx.req, ctx.res);
     ctx.respond = false;
     ctx.res.statusCode = 200;
     return;
