e10cdb0734f6c759b26f9e8e7459b4f2a79944f6
Ryan O'Shea <ryan.oshea@exitroute.org>
2020-10-20 22:19:28 +0200

    add cookies

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,11 @@ import Cookies from "js-cookie";
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
+    const config = {
+      apiKey: API_KEY,
+      shopOrigin: Cookies.get("shopOrigin"),
+      forceRedirect: true,
+    };
     return (
       <React.Fragment>
         <Head>
