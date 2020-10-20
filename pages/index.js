558bbe3474a1174295e14589c86a6fd7c32a7267
Ryan O'Shea <ryan.oshea@exitroute.org>
2020-10-20 23:17:06 +0200

    initialise apolloClitent and ProductList

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { EmptyState, Layout, Page } from "@shopify/polaris";
 import { ResourcePicker } from "@shopify/app-bridge-react";
 import store from "store-js";
+import ProductList from "../components/ProductList";
 
 function Index() {
   const [modal, setModal] = useState({ open: false });
@@ -25,16 +26,20 @@ function Index() {
       />
 
       <Layout>
-        <EmptyState
-          heading="Manage your inventory transfers"
-          action={{
-            content: "Select Products",
-            onAction: () => setModal({ open: true }),
-          }}
-          image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
-        >
-          <p>Select Products</p>
-        </EmptyState>
+        {emptyState ? (
+          <EmptyState
+            heading="Manage your inventory transfers"
+            action={{
+              content: "Select Products",
+              onAction: () => setModal({ open: true }),
+            }}
+            image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
+          >
+            <p>Select Products</p>
+          </EmptyState>
+        ) : (
+          <ProductList />
+        )}
       </Layout>
     </Page>
   );
