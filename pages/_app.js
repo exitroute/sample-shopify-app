2000ecd56c24e5143c60037d5f8f48ff2327ff71
Ryan O'Shea <ryan.oshea@exitroute.org>
2020-10-20 22:16:35 +0200

    next js app

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import App from "next/app";
+import Head from "next/head";
+import { AppProvider } from "@shopify/polaris";
+import { Provider } from "@shopify/app-bridge-react";
+import "@shopify/polaris/dist/styles.css";
+import translations from "@shopify/polaris/locales/en.json";
+import Cookies from "js-cookie";
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Sample App</title>
+          <meta charSet="utf-8" />
+        </Head>
+        <Provider config={config}>
+          <AppProvider i18={translations}>
+            <Component {...pageProps} />
+          </AppProvider>
+        </Provider>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MyApp;
